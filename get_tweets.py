e4014fd33b614d52e343f771d856314d1723d967
Ante Wall <antewall@gmail.com>
2015-03-03 11:34:25 +0100

    Added Relevant Tweets only json and parsing

--- get_tweets.py
+++ get_tweets.py
@@ -9,3 +9,6 @@ tf.save(tf.tweet_list,'closing')
 
 tp.delete_duplicates(tp.get_files_from_folder('./data'))
 
+from_date = datetime.datetime(2015,2,25)
+tp.find_relevant_tweets('./parsed/parsed_tweets.json',date=from_date)
+

326b9a9c96e78900b42fafae45e727ef02993f21
Dan Andreasson <dan@danandreasson.se>
2015-03-03 15:46:33 +0100

    Hasse is winning

--- main.py
+++ main.py
@@ -4,51 +4,47 @@ import re
 import json
 import pprint
 import math
+from collections import OrderedDict
 
 STOP_WORDS = "sissådär var att jag".split()
 BAD_CHARS = r"[,.?!]"
 
-pc = {}
-pw = {}
-
+artists = [
+        'Andreas Weise – ”Bring Out the Fire”',
+        'Linus Svenning – ”Forever Starts Today”',
+        'Hasse Andersson – ”Guld och gröna skogar”',
+        'Kristin Amparo – ”I See You”',
+        'Dolly Style – ”Hello Hi”',
+        'Dinah Nah – ”Make Me (La La La)”',
+        'Behrang Miri feat. Victor Crone – ”Det rår vi inte för”',
+        'Samir & Viktor - ”Groupie”',
+        ]
 UNKNOWN_WORDS = []
 
+chance_of_winning = {}
+
 class NaiveBayesClassifier():
+    pc = {}
+    pw = {}
+    nr_of_tweets = 0
 
     def save(self, filename):
         with open(filename, 'w') as outfile:
-            json.dump({"pw": pw, "pc": pc, outfile)
+            json.dump({"pw": self.pw, "pc": self.pc}, outfile)
 
     def load(self, filename):
         with open(filename) as fp:
             data = json.load(fp)
-            pw = data["pw"]
-            pc = data["pc"]
+            self.pw = data["pw"]
+            self.pc = data["pc"]
 
     def get_tokens(self, tweet):
         """Returns the token list for the specified tweet."""
         message = re.compile(BAD_CHARS, re.IGNORECASE).sub("", tweet["message"].lower())
         return [x for x in message.split() if x not in STOP_WORDS]
 
-    def is_positive(self, tweet):
-        """Returns true if a tweet is positive."""
-        pass
-
-    def get_artist(self, tweet):
-        """Returns the artist of the specified tweet."""
-        pass
-
-    def accuracy(self, tweets):
-        """Computes accuracy on the specified test data."""
-        pass
-
-    def precision(self, c, tweets):
-        """Computes precision for class `c` on the specified test data."""
-        pass
-
-    def recall(self, c, tweets):
-        """Computes recall for class `c` on the specified test data."""
-        pass
+    def placements(self):
+        return chance_of_winning
 
     def ensure_key(self, d, k, v):
         """If the key is not present, create it with v as value """
@@ -57,16 +53,27 @@ class NaiveBayesClassifier():
 
     def predict(self, tweet):
         """Predicts the artist of the specified tweet."""
+        self.nr_of_tweets += 1
         probable_artists = {}
-        pprint.pprint(pw)
-        for k in pw:
+        for k in self.pw:
             probable_artists[k] = 0
+            chance_of_winning[k] = 0
+
         for token in self.get_tokens(tweet):
             for artist in probable_artists:
-                if token in pw[artist].keys():
-                    print(pw[artist][token])
-                    probable_artists[artist] += (pw[artist][token])
-        pprint.pprint(probable_artists)
+                if token in self.pw[artist].keys():
+                    probable_artists[artist] += math.log2(self.pw[artist][token])
+                else:
+                    UNKNOWN_WORDS.append(token)
+
+        winner = None
+        for probable_artist, p in probable_artists.items():
+            if winner == None:
+                winner = probable_artist
+            elif p > probable_artists[winner]:
+                winner = probable_artist
+        chance_of_winning[winner] += probable_artists[winner]
+
 
 
     def train(self, tweets):
@@ -75,19 +82,18 @@ class NaiveBayesClassifier():
         for tweet in tweets:
             tokens = self.get_tokens(tweet)
             artist = tweet["artist"]
-            self.ensure_key(pc, artist, 0)
-            pc[artist] += 1
-            self.ensure_key(pw, artist, {})
+            self.ensure_key(self.pc, artist, 0)
+            self.pc[artist] += 1
+            self.ensure_key(self.pw, artist, {})
             for w in tokens:
-                self.ensure_key(pw[artist], w, 0)
-                pw[artist][w] += 1
+                self.ensure_key(self.pw[artist], w, 0)
+                self.pw[artist][w] += 1
                 self.ensure_key(word_count, w, 0)
                 word_count[w] += 1
 
-        for artist, words in pw.items():
+        for artist, words in self.pw.items():
             for word, c in words.items():
-                pw[artist][word] = c / word_count[word]
-        self.save()
+                self.pw[artist][word] = c / word_count[word]
 
 
 
@@ -126,7 +132,7 @@ if __name__ == "__main__":
     # Load a trained model from a file and evaluate it on test data.
     # Usage: python lab1.py evaluate MODEL_FILE TEST_DATA_FILE
     if sys.argv[1] == "evaluate":
-        classifier = MyNaiveBayesClassifier()
+        classifier = NaiveBayesClassifier()
         LOG("Loading the model from %s ..." % sys.argv[2])
         classifier.load(sys.argv[2])
         LOG(" done\n")
@@ -134,8 +140,11 @@ if __name__ == "__main__":
             LOG("Loading test data from %s ..." % sys.argv[3])
             test_data = json.load(fp)
             LOG(" done\n")
-        LOG("accuracy = %.4f\n" % classifier.accuracy(test_data))
-        for c in sorted(classifier.pc):
-            p = classifier.precision(c, test_data)
-            r = classifier.recall(c, test_data)
-            LOG("class %s: precision = %.4f, recall = %.4f\n" % (c, p, r))
+        for tweet in test_data:
+            classifier.predict(tweet)
+        for artist, points in classifier.placements().items():
+            print( "{0:.3f} {1}".format(abs(points), artists[int(artist)-1]))
+        print("\n" +str(classifier.nr_of_tweets) + " tweets was predicted")
+        print(str(len(set(UNKNOWN_WORDS))) + " ord skippades")
+        #for word in set(UNKNOWN_WORDS):
+        #    print(word, end=" ")
